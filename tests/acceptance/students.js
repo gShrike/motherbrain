0b2f551cb1073605814dd92031ecba2dccfcb69f
chadwithuhc <chadwithuhc@gmail.com>
2017-09-02 17:30:40 -0600

    Students: Acceptance test for new filters

--- tests/acceptance/students.js
+++ tests/acceptance/students.js
@@ -51,6 +51,56 @@ describe("acceptance - /students", function(){
             }).catch(done);
         });
     });
+    describe("#GET /students?github=kylecoberly", function(){
+        it("displays a list of matching serialized students", done => {
+            request(app)
+                .get(`${VERSION}/students?github=kylecoberly`)
+                .expect(200)
+            .then(response => {
+                assert.deepEqual(response.body.data, [{
+                    id: "1",
+                    type: "students",
+                    attributes: {
+                        "first-name": "Kyle",
+                        "last-name": "Coberly",
+                        "email": "kyle.coberly@gmail.com",
+                        "twitter": "kylecoberly",
+                        "phone": "777-777-7777",
+                        "github": "kylecoberly",
+                        "linkedin": "kylecoberly",
+                        "galvanize-id": "1",
+                        "avatar": "https://www.google.com"
+                    }
+                }]);
+                done();
+            }).catch(done);
+        });
+    });
+    describe("#GET /students?email=danny.fritz@gmail.com", function(){
+        it("displays a list of matching serialized students", done => {
+            request(app)
+                .get(`${VERSION}/students?email=danny.fritz@gmail.com`)
+                .expect(200)
+            .then(response => {
+                assert.deepEqual(response.body.data, [{
+                    id: "2",
+                    type: "students",
+                    attributes: {
+                        "first-name": "Danny",
+                        "last-name": "Fritz",
+                        "email": "danny.fritz@gmail.com",
+                        "twitter": "dannyfritz",
+                        "phone": "777-777-7777",
+                        "github": "dannyfritz",
+                        "linkedin": "dannyfritz",
+                        "galvanize-id": "2",
+                        "avatar": "https://www.google.com"
+                    }
+                }]);
+                done();
+            }).catch(done);
+        });
+    });
     describe("#GET /students/:id", function(){
         it("displays a single serialized student", done => {
             request(app)
